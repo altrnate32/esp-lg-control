0de929b65e1820983570b1c88741820bb92cdc4e
altrnate32 <john@doe.org>
2022-11-28 21:40:53 +0100

    ****Warning->Read full message ***** Rewrite of algoritm to use finite state machine. Algoritm itself unchanged ****Warning*** Does compile, but is not tested yet (not even uploaded) Will probably not function at all at this moment, wait until I have tested

---
+++ lg_heatpump_functions.h
@@ -0,0 +1,73 @@
+    static const char* state_string[] = {"Init","Idle","Start","Starting","EarlyRun","Running","Overshooting","Undershooting","Waiting","HotWater","Halt","Afterrun"};
+    enum States {INIT,IDLE,START,STARTING,EARLYRUN,RUNNING,OVERSHOOTING,UNDERSHOOTING,WAITING,SWW,HALT,AFTERRUN};
+
+    //update target temp through modbus
+    void set_target_temp(float target){
+      auto water_temp_call = id(water_temp_target_output).make_call();
+      water_temp_call.set_value(round(target));
+      water_temp_call.perform();
+      ESP_LOGD("set_target_temp", "Modbus target set to: %f", round(target));
+    }
+    
+    void calculate_derivative(float tracking_value,std::vector<float>* derivative, float* derivative_D_5,float* derivative_D_10){
+      //calculate derivative
+      float d_number = tracking_value;
+      derivative->push_back(d_number);
+      //limit size to 31 elements(30 minutes)
+      if(derivative->size() > 31) derivative->erase(derivative->begin());
+      //calculate current derivative for 5 and 10 minutes
+      //derivative is measured in degrees/minute
+      *derivative_D_5 = 0;
+      *derivative_D_10 = 0;
+      if(derivative->size() > 5){   
+        *derivative_D_5 = (derivative->back()-derivative->at(derivative->size()-6))/5;
+      }
+      if(derivative->size() > 10){   
+      *derivative_D_10 = (derivative->back()-derivative->at(derivative->size()-11))/10;
+      id(derivative_value).publish_state(*derivative_D_10*60);
+     }
+    }
+  
+    bool check_switch_off(States state, uint32_t run_time, uint32_t run_start_time){
+      if(id(thermostat_signal).state) return false; //thermostat still requesting heat
+      //thermostat switched off
+      //if not actually running, go straight to halt
+      if(!id(compressor_running).state) return true;
+      //Else check minimum run time has passed
+      if((run_time - run_start_time) > (id(minimum_run_time).state * 60)) return true;
+      return false;
+    }
+  
+    //calculate stooklijn function
+    void calculate_stooklijn(){
+      //Calculate stooklijn target
+      //Hold previous script run oat value
+      static float prev_oat = 15; //oat above minimum water temp to prevent strange events on startup
+      //wait for a valid oat reading
+      float oat = 15;
+      if(id(buiten_temp).state > 60 || id(buiten_temp).state < -50 || isnan(id(buiten_temp).state)){
+        oat = prev_oat;
+        //use prev_oat (or 15) and do not set update_stooklijn to false, to trigger a new run on next cycle
+        ESP_LOGD("calculate_stooklijn", "Invalid OAT (%f) waiting for next run", id(buiten_temp).state);
+      }  else {
+        oat = round(id(buiten_temp).state);
+        prev_oat = oat;
+        id(update_stooklijn) = false;
+      }
+
+      //Formula is wTemp = (Z x (stooklijn_start_temp - OAT)) + stooklijn_min_wtemp         
+      //Formula to calculate Z = 0-((stooklijn_max_wtemp-stooklijn_min_wtemp)) / (stooklijn_min_oat - stooklijn_start_temp))
+      const float Z =  0 - (float)((id(stooklijn_max_wtemp).state-id(stooklijn_min_wtemp).state)/(id(stooklijn_min_oat).state - id(stooklijn_start_temp).state));
+      //If oat below minimum oat, clamp to minimum value
+      clamp(oat,id(stooklijn_min_oat).state,id(stooklijn_start_temp).state);
+      id(stooklijn_target) = (int)round( (Z * (id(stooklijn_start_temp).state-oat))+id(stooklijn_min_wtemp).state);
+      //Add stooklijn offset
+      id(stooklijn_target) = id(stooklijn_target) + id(wp_stooklijn_offset).state;
+      //Clamp target to minimum temp/max water+3
+      clamp(id(stooklijn_target),id(stooklijn_min_wtemp).state,id(stooklijn_max_wtemp).state+3);
+      ESP_LOGD("calculate_stooklijn", "Stooklijn calculated with oat: %f, Z: %f, offset: %f, result: %f",oat, Z, id(wp_stooklijn_offset).state, id(stooklijn_target));
+      //Publish new stooklijn value to watertemp value sensor
+      id(watertemp_target).publish_state(id(stooklijn_target));
+      return;
+    }
+
